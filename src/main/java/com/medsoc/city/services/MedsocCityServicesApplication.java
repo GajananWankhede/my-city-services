ad5d1851423c0ac1757064d162ae574b0d99ccc1
Sushant sinha <sushant.sinha2013@gmail.com>
2023-02-20 23:27:12 +0530

    intial commit

---
+++ src/main/java/com/medsoc/city/services/MedsocCityServicesApplication.java
@@ -0,0 +1,13 @@
+package com.medsoc.city.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsocCityServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedsocCityServicesApplication.class, args);
+    }
+
+}
